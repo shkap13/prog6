d889023af828eee777c7a335e7be291194ba8e27
shkap13 <shreyakappala@gmail.com>
2022-11-03 14:30:30 -0500

    have a lookup

--- src/main/java/assignment/Node.java
+++ src/main/java/assignment/Node.java
@@ -1,5 +1,65 @@
 package assignment;
 
-public class Node {
+public class Node<K,V> {
+    private int priorityValue;
+    private K key;
+    private V value;
+    private Node<K,V> left;
+    private Node<K,V> right;
+
+    public Node(){
+        priorityValue = 0;
+        key = null;
+        value = null;
+        left = null;
+        right = null;
+    }
+
+    public Node(int pri, K k, V v, Node<K,V> l, Node<K,V> r){
+        priorityValue = pri;
+        key = k;
+        value = v;
+        left = l;
+        right = r;
+    }
+
+    public void setPriorityValue(int input){
+        priorityValue = input;
+    }
+
+    public void setKey(K k){
+        key = k;
+    }
+
+    public void setValue(V v){
+        value = v;
+    }
+
+    public void setLeft(Node<K,V> a){
+        left = a;
+    }
+
+    public void setRight(Node<K,V> a){
+        right = a;
+    }
     
+    public int getPriorityValue(){
+        return priorityValue;
+    }
+
+    public K getKey(){
+        return key;
+    }
+
+    public V getValue(){
+        return value;
+    }
+
+    public Node<K,V> getLeft(){
+        return left;
+    }
+
+    public Node<K,V> getRight(){
+        return right;
+    }
 }
--- src/main/java/assignment/TreapMap.java
+++ src/main/java/assignment/TreapMap.java
@@ -1,5 +1,133 @@
 package assignment;
 
-public class TreapMap {
+import java.util.Iterator;
+import java.util.Random;
+
+public class TreapMap<K extends Comparable<K>, V> implements Treap<K,V>{
+    Node<K,V> root;
+
+    public TreapMap(){
+        root = null;
+    }
+
+    public TreapMap(Node<K,V> newNode){
+        root = newNode;
+    }
+
+    @Override
+    public V lookup(K key) {
+        Node<K,V> found = findNode(key);
+
+        if(found.getKey().compareTo(key) == 0){
+            return found.getValue();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void insert(K key, V value) {
+        //generate priority value
+
+        //assuming no duplicates at the moment
+        Node<K,V> parentNode = findNode(key);
+
+        if(key.compareTo(parentNode.getKey()) < 0){
+
+        }
+        
+        
+    }
+
+    @Override
+    public Object remove(Comparable key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Treap[] split(Comparable key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void join(Treap t) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void meld(Treap t) throws UnsupportedOperationException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void difference(Treap t) throws UnsupportedOperationException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Iterator iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double balanceFactor() throws UnsupportedOperationException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public void printTreeInOrder(Node<K,V> root){
+        if(root.getLeft() != null){
+            printTreeInOrder(root.getLeft());
+        }
+
+        System.out.println("priority value: " + root.getPriorityValue() + ", key is: " + root.getKey() + ", value is: " + root.getValue());
+
+        if(root.getRight() != null){
+            printTreeInOrder(root.getRight());
+        }
+    }
+
+    //returns node with same key 
+    //if key is not in tree, returns the parent node
+    public Node<K,V> findNode(K key){
+        Node<K,V> current = root;
+
+        while(true){
+
+            //if same key is found, the node is returned
+            if(key.compareTo(current.getKey()) == 0){
+                return current;
+            }
+
+            //if key is less than current key
+            else if((key.compareTo(current.getKey()) < 0)){
+                if(current.getLeft() == null){
+                    return current;
+                }
+                current = current.getLeft();
+            }
+
+            //if key is greater than current key
+            else{
+                if(current.getRight() == null){
+                    return current;
+                }
+                current = current.getRight();
+            }
+        }        
+    }
+
+    public int generatePriority(){
+        Random rand = new Random();
+        int intRandom = rand.nextInt(MAX_PRIORITY);
+        
+        return intRandom;
+    }
     
 }
